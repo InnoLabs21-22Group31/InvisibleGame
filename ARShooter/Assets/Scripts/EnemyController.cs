dff58ac3555f2496369c764009c65db25368ddf7
DrianH <84391221+DrianH@users.noreply.github.com>
2022-03-29 17:12:54 +0200

    Revert "I23-25: Human assets + walking towards player camera"

--- ARShooter/Assets/Scripts/EnemyController.cs
+++ ARShooter/Assets/Scripts/EnemyController.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class EnemyController : MonoBehaviour {
     public float speed = 5f;
@@ -12,36 +10,18 @@ public class EnemyController : MonoBehaviour {
     private GameObject playerCamera;
 
     private Vector3 targetPosition;
-
-    bool close = false;
     
     // Start is called before the first frame update
     void Start() {
         animator = gameObject.GetComponent<Animator>();
-        playerCamera = GameObject.Find("Main Camera");
+        playerCamera = GameObject.Find("PlayerCamera");
         targetPosition = playerCamera.transform.position;
         targetPosition.y = 0;
     }
 
     // Update is called once per frame
     void Update() {
-        if (!close) {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-            transform.LookAt(targetPosition);
-            var thisRotation = transform.rotation;
-            transform.Rotate(thisRotation.x, thisRotation.y + 90, thisRotation.z);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("EnemyRadius"))
-        {
-            close = true;
-        }
-        else
-        {
-            close = false;
-        }
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        transform.LookAt(targetPosition);
     }
 }
