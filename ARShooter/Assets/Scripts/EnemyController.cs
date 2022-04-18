8347231829388eda6a6fccf834c9fa96b198b163
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-04-18 14:10:36 +0200

    feat: Enemies stop in front of player. Movement now with Ridigbodies.

--- ARShooter/Assets/Scripts/EnemyController.cs
+++ ARShooter/Assets/Scripts/EnemyController.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+using System;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class EnemyController : MonoBehaviour {
     public float speed = 5f;
@@ -13,34 +10,40 @@ public class EnemyController : MonoBehaviour {
 
     private Vector3 targetPosition;
 
+    private Rigidbody _rigidbody;
+
     bool close = false;
-    
+
     // Start is called before the first frame update
     void Start() {
         animator = gameObject.GetComponent<Animator>();
-        playerCamera = GameObject.Find("Main Camera");
+        playerCamera = GameObject.Find("PlayerCamera");
         targetPosition = playerCamera.transform.position;
         targetPosition.y = 0;
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
         if (!close) {
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
             transform.LookAt(targetPosition);
             var thisRotation = transform.rotation;
             transform.Rotate(thisRotation.x, thisRotation.y + 90, thisRotation.z);
+            _rigidbody.AddRelativeForce(new Vector3(-1, 0, 0) * speed * Time.deltaTime,ForceMode.Force);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("EnemyRadius"))
-        {
+    private void StopMovement() {
+        _rigidbody.velocity = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            Debug.Log("Game over");
             close = true;
+            StopMovement();
         }
-        else
-        {
+        else {
             close = false;
         }
     }
