c100c885030c495f953bdd7ffd03110ef47fd3e5
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-04-19 12:11:35 +0200

    feat: Muzzle flash, weapon cd,

--- ARShooter/Assets/Scripts/Gun.cs
+++ ARShooter/Assets/Scripts/Gun.cs
@@ -3,17 +3,22 @@ using UnityEngine;
 public class Gun : MonoBehaviour {
     public Camera playerCamera;
     public float range = 100f;
+    public float rateOfFire = 1f;
+    public ParticleSystem muzzleFlash;
+
+    private float gunReadyAt = 0f;
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButton("Fire1")) {
+    void Update() {
+        if (Input.GetButton("Fire1") && Time.time >= gunReadyAt) {
             Fire();
         }
     }
 
     private void Fire() {
         RaycastHit hit;
+        muzzleFlash.Play();
+        gunReadyAt = Time.time + 1f / rateOfFire;
 
         // Fire straight ahead
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range)) {
