31540ad747fb0fb118ea28860625f41767a11c06
PRenkel <60927909+PRenkel@users.noreply.github.com>
2022-04-19 12:37:09 +0200

    feat: Reloading

---
+++ ARShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public TextMeshProUGUI ammoDisplay;
+
+    public void SetAmmoDisplay(int loadedAmmo, int ammoCapacity) {
+        ammoDisplay.SetText("{0} / {1}", loadedAmmo, ammoCapacity);
+    }
+}
