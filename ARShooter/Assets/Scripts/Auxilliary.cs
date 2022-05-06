921ffb19c11e0ca06bf9b1b93a7d5b25e2146704
DrianH <84391221+DrianH@users.noreply.github.com>
2022-05-06 09:53:22 +0200

    Describe Auxilliary
    Auxilliary has methods that belong to the general game loop.

--- ARShooter/Assets/Scripts/Auxilliary.cs
+++ ARShooter/Assets/Scripts/Auxilliary.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//You can insert here methods that aren't specific to anything, e.g the RestartGame method.
+
 public class Auxilliary : MonoBehaviour
 {
     // Start is called before the first frame update
